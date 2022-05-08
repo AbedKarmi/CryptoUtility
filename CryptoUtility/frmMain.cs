4fc41a41692f84fb246f31a3d4f25fc3e734c62d
AbedKarmi <abedkarmi@gmail.com>
2022-05-08 19:06:19 +0300

    upd

--- CryptoUtility/frmMain.cs
+++ CryptoUtility/frmMain.cs
@@ -930,9 +930,9 @@ Encoding method : Arabic Common CharSet Order [ACCO]";
 
 Quran was revealed to the last prophet Mohammad peace upon him. 
 
-Nowadays, almost all important data is being verified againest modification or corruption.
+Nowadays, almost all important data is being verified againest alteration and/or corruption.
 
-Quran, is protected against change, as God stated in the Quran. We believe that, Quran must have a way thant allows verification of its content.
+Quran, is being protected by God against any change, that was stated clearly in the Quran verse. We believe that, Quran must have a modern way that allows verification of its content.
 
 Text to byte conversion:
 We implemented several encoding options to convert Arabic text of the Quran to numeric stream of data. 
